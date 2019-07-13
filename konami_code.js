4c50614454bc82ccfb24c45b9ec6da6e4ba2abff
cjoannes <cjoannes@tulane.edu>
2019-07-13 16:16:13 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,5 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
-}
+function init() { let index=0; if(key===codes[index]) {index++; if (index===codes.length) {alert("Hurray!"); index = 0 } } else { index = 0;}}
+function init() {document.body.addEventListener('keydown',function(e){ var key = parseInt(e.which || e.detail);   var index=0;   if(key === code[index]){index++;  if(index === code.length){  alert("Hurray"); index=0;} }else{  console.log(e.which);  index=0;}})}
